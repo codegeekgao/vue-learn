78bb201c0c2f9604cd5d67bd84a7e4280a30729b
codegeekgao <codegeek_mrgao@163.com>
2021-06-10 21:48:21 +0800

    :sparkles: webpack | common Js

---
+++ day9/dist/bundle.js
@@ -0,0 +1,151 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__day8_module_js_b__ = __webpack_require__(2);
+const {add,divide} = __webpack_require__(1)
+
+console.log(add(1,2),divide(10,2))
+
+console.log(__WEBPACK_IMPORTED_MODULE_0__day8_module_js_b__["a" /* default */].person)
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+function add(a, b) {
+    return a + b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+module.exports= {
+    add,divide
+}
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* unused harmony export flag */
+/* unused harmony export sum */
+/* unused harmony export person */
+/* unused harmony export flag1 */
+/* unused harmony export sum1 */
+/* unused harmony export person1 */
+let flag = false
+
+function sum(a, b) {
+    return a + b
+}
+
+let name = '张三'
+let age = 3
+let salary = 23000
+
+let person = {
+    name,
+    age,
+    salary
+}
+
+// export default function () {
+//     return 'default function'
+// }
+
+ let flag1 = false
+ function sum1(a, b) {
+    return a + b
+}
+
+let name1 = '张三'
+let age1 = 3
+let salary1 = 23000
+
+ let person1 = {
+    name1,
+    age1,
+    salary1
+}
+
+
+// export default 默认只能有一个
+/* harmony default export */ __webpack_exports__["a"] = ({flag,sum,person});
+
+
+
+
+/***/ })
+/******/ ]);
---
+++ day9/src/main.js
@@ -0,0 +1,5 @@
+const {add,divide} = require('./mathUtls')
+import p from '../../day8/module/js/b'
+console.log(add(1,2),divide(10,2))
+
+console.log(p.person)
