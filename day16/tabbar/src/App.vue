8de731366643cd7f0b35b2447e2c89b271dbb810
codegeekgao <codegeek_mrgao@163.com>
2021-06-28 22:38:51 +0800

    :sparkles: create promise

---
+++ day16/tabbar/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  },
+  created() {
+    // eslint-disable-next-line no-unused-vars
+    let promise = new Promise((resolve,reject)=> {
+      // 进行异步请求，例如向后台请求数据
+       let success = false
+      if(success) {
+        console.log('我是请求成功后的逻辑')
+        resolve('请求成功')
+      } else {
+        console.log('我是请求失败后的逻辑')
+        reject('请求失败')
+      }
+    }).then(res=> console.log(res)).catch(err=> console.log(err))
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ day16/tabbar/src/store/index.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+// 使用Vuex插件
+Vue.use(Vuex)
