de20d5a23b75777e0f03a86fcc148a67b4986aa1
codegeekgao <codegeek_mrgao@163.com>
2021-07-03 16:50:08 +0800

    :sparkles: a express Demo

---
+++ express-learn/routes/express.js
@@ -0,0 +1,12 @@
+const Express = require('express')
+const app = new Express()
+
+app.get("/",((req, res) => {
+    res.send('hello express')
+}))
+
+const server = app.listen(3000,()=> {
+    const host = server.address().host
+    const port = server.address().port
+    console.log('http run at:%s:%d',host,port)
+})
